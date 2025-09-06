c68bbca7c577ccb0b03e5580d045989807dcc2eb
Allard van Helbergen <avanhelbergen@securecodewarrior.com>
2025-09-06 22:38:54 +1000

    Delete the format date lib that wasn't used.

--- src/lib/format-date.ts
+++
@@ -1,6 +0,0 @@
-import { format } from 'date-fns';
-
-export function formatDateRFC822(date: Date | string): string {
-  const d = typeof date === 'string' ? new Date(date) : date;
-  return format(d, 'EEE, dd MMM yyyy');
-}
